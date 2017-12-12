c767c8a4d57e9b239364e508302917e4c4999b09
eoinwoods <eoin.woods@artechra.com>
2017-12-12 21:57:33 +0000

    Comment to clarify network address and container ID formats

--- src/main/kotlin/com/artechra/apollo/netinfo/NetInfo.kt
+++ src/main/kotlin/com/artechra/apollo/netinfo/NetInfo.kt
@@ -2,6 +2,29 @@ package com.artechra.apollo.netinfo
 
 typealias ContainerAddressMap = Map<String, String>
 typealias AddressContainerMap = Map<String, String>
+
+/**
+ * The interface to the internal service that maps between Docker container IDs
+ * and IP based network addresses.
+ *
+ * The interface doesn't try to create complex types for these parameters and both
+ * are treated as strings, so some care is necessary.
+ *
+ * Container IDs are either short (12 lowercase hex digits e.g. "ad6d5d32576a")
+ * or long (64  lowercase hex digits
+ * e.g. "ad6d5d32576ad3cb1fcaa59b564b8f6f22b079631080ab1a3bbac9199953eb7d")
+ *
+ * The implementation of this interface will take reasonable care to match
+ * containers as well as it can, by converting upper case to lower case hex
+ * digits and for short IDs trying to match them as the prefix of a long ID
+ * if a short ID doesn't match.
+ *
+ * Network addresses are the IPv4 address in dotted form plus a port number,
+ * following the customary format "192.168.1.2:123"  If the port number is
+ * unknown or not significant then "0" can be used and this will become a
+ * wild card when matching.
+ *
+ */
 interface NetInfo {
     fun getAddressesForContainers() : ContainerAddressMap
     fun getContainersForAddresses() : AddressContainerMap
