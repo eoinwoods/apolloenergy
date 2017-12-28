39566abba4f8b42f30337c92a65bf9536259285a
eoinwoods <eoin.woods@artechra.com>
2017-12-28 19:49:50 +0000

    Add more useful setup scripts

--- bin/apollo-docker-setup.sh
+++ bin/apollo-docker-setup.sh
@@ -1,6 +1,7 @@
 #
-# apollo-env-setup.sh - setup Apollo runtime environment on empty Ubuntu (AWS) VM using
-#                       manual "docker run" commands rather than "docker-compose"
+# apollo-docker-setup.sh - upgrade Ubuntu OS and install docker and 1.80.0 of docker-compose
+#                          to allow the Apollo environment to be started using docker-compose
+#                          and the apollo-env.yml compose file
 #
 
 sudo apt-get update -y
@@ -25,15 +26,3 @@ sudo docker run hello-world
 sudo curl -L https://github.com/docker/compose/releases/download/1.18.0/docker-compose-`uname -s`-`uname -m` -o /usr/local/bin/docker-compose
 sudo chmod +x /usr/local/bin/docker-compose
 sudo docker-compose --version
-
-# https://zipkin.io/pages/quickstart
-# Remember the -d which means "detach" to run in background
-sudo docker run -d --name zipkinserver -p 9411:9411 openzipkin/zipkin
-
-# https://hub.docker.com/_/telegraf/
-sudo docker run -d --name influxdb -p 8083:8083 -p 8086:8086 influxdb
-# Need to copy apollo_telegraf.conf into place as part of setup
-sudo docker run -d --net=container:influxdb --name telegraf -v /var/run/docker.sock:/var/run/docker.sock -v ~/etc/apollo_telegraf.conf:/etc/telegraf/telegraf.conf:ro telegraf
-
-# -it == interactive and allocate a tty
-sudo docker run -it --net=container:influxdb influxdb influx
