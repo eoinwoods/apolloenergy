e63bf7ead28d1b2cb75e015feaef49482b35aee7
eoinwoods <eoin.woods@artechra.com>
2018-01-19 22:45:03 +0000

    Remove Kotlin warnings

--- src/test/kotlin/com/artechra/apollo/integration/TestInfluxDbDecorator.kt
+++ src/test/kotlin/com/artechra/apollo/integration/TestInfluxDbDecorator.kt
@@ -13,13 +13,15 @@ import kotlin.test.assertEquals
 class TestInfluxDbDecorator {
     val LOG = Logger.getLogger(this.javaClass.name)
 
-    private val DBURL    = IntegrationTestConstants.INFLUX_URL
-    private val DATABASE = IntegrationTestConstants.DB_NAME
+    companion object {
+        private val DBURL               = IntegrationTestConstants.INFLUX_URL
+        private val DATABASE            = IntegrationTestConstants.DB_NAME
 
-    private val CONTAINER_ID        = IntegrationTestConstants.GATEWAY_CONTAINER_ID
-    private val DISKIO_CONTAINER_ID = IntegrationTestConstants.INFLUXDB_CONTAINER_ID
-    private val SPAN_TIME_MS        = IntegrationTestConstants.SPAN_START_TIME_MS
-    private val TEST_SET            = IntegrationTestConstants.TEST_SET_NAME
+        private val CONTAINER_ID        = IntegrationTestConstants.GATEWAY_CONTAINER_ID
+        private val DISKIO_CONTAINER_ID = IntegrationTestConstants.INFLUXDB_CONTAINER_ID
+        private val SPAN_TIME_MS        = IntegrationTestConstants.SPAN_START_TIME_MS
+        private val TEST_SET            = IntegrationTestConstants.TEST_SET_NAME
+    }
 
     private var influxdb: InfluxDbDecorator? = null
 
@@ -35,18 +37,16 @@ class TestInfluxDbDecorator {
 
     // A little helper to work around the need for mutability in the decorator reference
     // due to the use of @Before rather than an init {} block
-    fun getDbConn() : InfluxDbDecorator {
-        val dbconn = this.influxdb
-        if (dbconn == null) throw IllegalStateException("No database connection available")
-        return dbconn
+    private fun getDbConn() : InfluxDbDecorator {
+        return influxdb ?: throw IllegalStateException("No database connection available")
     }
 
     @Test
     fun testThatTestDataSet3IsLoaded() {
         val query = Query("SELECT * FROM apollo_check WHERE value = 1", DATABASE)
         val dbconn = getDbConn().getInfluxDbConnection()
-        val result = dbconn?.query(query)
-        assertEquals("data_set", result?.results?.get(0)?.getSeries()?.get(0)?.getColumns()?.get(2))
+        val result = dbconn.query(query)
+        assertEquals("data_set", result?.results?.get(0)?.series?.get(0)?.columns?.get(2))
         // This is a little arcane but is a reflection of InfluxDB's quite complex result set structure
         val resultValue = result!!.results[0]!!.series[0]!!.values[0][2] as String
         assertEquals(TEST_SET, resultValue)
@@ -56,7 +56,7 @@ class TestInfluxDbDecorator {
     fun testThatCpuMeasurementMappingIsCorrect() {
         val query = Query("SELECT time, container_name, usage_total FROM docker_container_cpu ORDER BY time LIMIT 1", DATABASE)
         val dbconn = getDbConn().getInfluxDbConnection()
-        val result = dbconn?.query(query)
+        val result = dbconn.query(query)
         val resultMapper = InfluxDBResultMapper()
         val cpuList = resultMapper.toPOJO(result, CpuMeasurement::class.java)
         LOG.info("cpuList=" + cpuList[0])
@@ -70,7 +70,7 @@ class TestInfluxDbDecorator {
     fun testThatMemMeasurementMappingIsCorrect() {
         val query = Query("SELECT time, container_name, usage FROM docker_container_mem ORDER BY time LIMIT 1", DATABASE)
         val dbconn = getDbConn().getInfluxDbConnection()
-        val result = dbconn?.query(query)
+        val result = dbconn.query(query)
         val resultMapper = InfluxDBResultMapper()
         val memList = resultMapper.toPOJO(result, MemMeasurement::class.java)
         assertEquals(1, memList.size)
@@ -84,7 +84,7 @@ class TestInfluxDbDecorator {
     fun testThatDiskIoMeasurementMappingIsCorrect() {
         val query = Query("SELECT time, container_name, io_service_bytes_recursive_total FROM docker_container_blkio ORDER BY time LIMIT 1", DATABASE)
         val dbconn = getDbConn().getInfluxDbConnection()
-        val result = dbconn?.query(query)
+        val result = dbconn.query(query)
         val resultMapper = InfluxDBResultMapper()
         val diskIoList = resultMapper.toPOJO(result, DiskIoMeasurement::class.java)
         assertEquals(1, diskIoList.size)
@@ -99,7 +99,7 @@ class TestInfluxDbDecorator {
         val query = Query("SELECT time, container_name, rx_bytes, tx_bytes FROM docker_container_net " +
                                     "WHERE time = 1515237462000000000 ORDER BY time LIMIT 1", DATABASE)
         val dbconn = getDbConn().getInfluxDbConnection()
-        val result = dbconn?.query(query)
+        val result = dbconn.query(query)
         val resultMapper = InfluxDBResultMapper()
         val netIoList = resultMapper.toPOJO(result, NetIoMeasurement::class.java)
         assertEquals(1, netIoList.size)
