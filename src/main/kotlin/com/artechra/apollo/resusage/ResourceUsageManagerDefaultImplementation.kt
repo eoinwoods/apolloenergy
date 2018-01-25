3d971d6dcf6b875e12211f1d028180fc9a0544b0
eoinwoods <eoin.woods@artechra.com>
2018-01-25 22:42:33 +0000

    Refactor to rename parameters to avoid warnings

--- src/main/kotlin/com/artechra/apollo/resusage/ResourceUsageManagerDefaultImplementation.kt
+++ src/main/kotlin/com/artechra/apollo/resusage/ResourceUsageManagerDefaultImplementation.kt
@@ -4,7 +4,7 @@ import com.artechra.apollo.types.ResourceUsage
 import com.artechra.apollo.types.ResourceUsageMetric
 
 class ResourceUsageManagerDefaultImplementation : ResourceUsageManager {
-    override fun getResourceUsage(containerId : String, startTime : Long, endTime : Long) : ResourceUsageMetric {
+    override fun getResourceUsage(containerId : String, startTimeMsec : Long, endTimeMsec : Long) : ResourceUsageMetric {
         return ResourceUsageMetric(1234567890, "b38b452428f7", ResourceUsage(2000, 200, 123, 456))
     }
 
