4db85f0f42f733a56c95bfcbf2940cec93b283a5
eoinwoods <eoin.woods@artechra.com>
2018-01-25 23:18:07 +0000

    Fixed stub for move to msec precision

--- src/main/kotlin/com/artechra/apollo/traces/TraceManagerDefaultImplementation.kt
+++ src/main/kotlin/com/artechra/apollo/traces/TraceManagerDefaultImplementation.kt
@@ -4,7 +4,8 @@ import com.artechra.apollo.types.Span
 import com.artechra.apollo.types.Trace
 
 class TraceManagerDefaultImplementation : TraceManager {
-    val exampleTrace = Trace(setOf(Span("54C92796854B15C8", "54C92796854B15C8", "10.10.1.1", 10000, 20000)))
+    val baseTime = 1515237372000
+    val exampleTrace = Trace(setOf(Span("54C92796854B15C8", "54C92796854B15C8", "10.10.1.1", baseTime+10000, baseTime+20000)))
 
     override fun getTrace(traceId: String): Trace {
         return exampleTrace
