b5d5bfd0ac3ebb45e39146d087d9ba848efe69be
eoinwoods <eoin.woods@artechra.com>
2018-02-03 21:19:31 +0000

    Add new ctor param

--- src/main/kotlin/com/artechra/apollo/archdesc/ArchitectureManagerDefaultImpl.kt
+++ src/main/kotlin/com/artechra/apollo/archdesc/ArchitectureManagerDefaultImpl.kt
@@ -4,5 +4,5 @@ import com.artechra.apollo.types.ArchitecturalDescription
 import com.artechra.apollo.types.ArchitecturalElement
 
 class ArchitectureManagerDefaultImpl : ArchitectureManager {
-    override fun getStructure(): ArchitecturalDescription = ArchitecturalDescription(emptySet<ArchitecturalElement>())
+    override fun getStructure(): ArchitecturalDescription = ArchitecturalDescription("System1", emptySet<ArchitecturalElement>())
 }
