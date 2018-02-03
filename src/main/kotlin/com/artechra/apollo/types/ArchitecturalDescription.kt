d5b56959f2f65ca7c2d497e6065c68b0087e16a9
eoinwoods <eoin.woods@artechra.com>
2018-02-03 23:03:29 +0000

    Remove unused imports

--- src/main/kotlin/com/artechra/apollo/types/ArchitecturalDescription.kt
+++ src/main/kotlin/com/artechra/apollo/types/ArchitecturalDescription.kt
@@ -1,10 +1,5 @@
 package com.artechra.apollo.types
 
-import com.beust.klaxon.JsonArray
-import com.beust.klaxon.JsonObject
-import com.beust.klaxon.Parser
-import java.io.File
-
 data class ArchitecturalDescription(val name : String, val elements : Set<ArchitecturalElement>) {
     fun findElementByName(name : String) : ArchitecturalElement? {
         elements.forEach {
