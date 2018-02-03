d178b46a65c17b4f423dff57d10aaf2bdf9ac167
eoinwoods <eoin.woods@artechra.com>
2018-02-03 22:46:10 +0000

    Formatting correction

--- src/test/kotlin/com/artechra/apollo/netinfo/TestNetInfoDockerJsonImpl.kt
+++ src/test/kotlin/com/artechra/apollo/netinfo/TestNetInfoDockerJsonImpl.kt
@@ -8,22 +8,26 @@ import kotlin.test.assertEquals
 
 class TestNetInfoDockerJsonImpl {
 
+    fun defaultDataFile() : String {
+        return getDataFilePath("test_docker_network.json")
+    }
+
     @Test
     fun testInitialisationLoadsData() {
-        val obj = NetInfoDockerJsonImpl(getDataFilePath("test_docker_network.json"))
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
         assertEquals(6, obj.getNumberOfContainers(), "Wrong number of containers loaded")
     }
 
     @Test
     fun testExpectedContainerHasNetworkAddress() {
-        val obj = NetInfoDockerJsonImpl(getDataFilePath("test_docker_network.json"))
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
         val influxAddr = obj.getAddressForContainerId(IntegrationTestConstants.INFLUXDB_CONTAINER_ID)
         assertEquals("172.18.0.3", influxAddr, "Wrong address for InfluxDb container")
     }
 
     @Test
     fun testExpectedContainerHasName() {
-        val obj = NetInfoDockerJsonImpl(getDataFilePath("test_docker_network.json"))
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
         val influxAddr = obj.getNameForContainerId(IntegrationTestConstants.INFLUXDB_CONTAINER_ID)
         assertEquals("influxdb", influxAddr, "Wrong name for InfluxDb container")
     }
@@ -31,17 +35,16 @@ class TestNetInfoDockerJsonImpl {
 
     @Test
     fun testNetworkAddressResolvesToCorrectContainer() {
-        val obj = NetInfoDockerJsonImpl(getDataFilePath("test_docker_network.json"))
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
         val gatewayAddr = obj.getContainerIdWithAddress("172.18.0.7")
         assertEquals(IntegrationTestConstants.GATEWAY_CONTAINER_ID, gatewayAddr, "Wrong ID for Gateway container address")
     }
 
     @Test
     fun testContainerNameResolvesToCorrectContainer() {
-        val obj = NetInfoDockerJsonImpl(getDataFilePath("test_docker_network.json"))
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
         val gatewayName = obj.getContainerIdWithName("gateway")
         assertEquals(IntegrationTestConstants.GATEWAY_CONTAINER_ID, gatewayName, "Wrong ID for Gateway container name")
-
     }
 
 }
