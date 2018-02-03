fb54dbbf5b0beb458400f1635001d106c51b110c
eoinwoods <eoin.woods@artechra.com>
2018-02-03 00:01:13 +0000

    Added run script for Energy Apollo

---
+++ run.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+#
+# Simple run script for EnergyApollo to run it via the "fat" jar and using
+# the default properties file
+#
+java -jar build/libs/EnergyApollo-all.jar etc/apollo.properties
