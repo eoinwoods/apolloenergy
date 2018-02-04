6c6d5679de3d5d6c3c0e7ccad93bb317f6b9547e
eoinwoods <eoin.woods@artechra.com>
2018-02-04 18:25:13 +0000

    Added creation of synthetic spans for architectural elements

--- src/main/kotlin/com/artechra/apollo/calculator/EnergyCalculatorImpl.kt
+++ src/main/kotlin/com/artechra/apollo/calculator/EnergyCalculatorImpl.kt
@@ -5,6 +5,9 @@ import com.artechra.apollo.netinfo.NetInfo
 import com.artechra.apollo.resusage.ResourceUsageManager
 import com.artechra.apollo.types.Trace
 import com.artechra.apollo.traces.TraceManager
+import com.artechra.apollo.types.ArchitecturalDescription
+import com.artechra.apollo.types.ArchitecturalElement
+import com.artechra.apollo.types.Span
 import org.apache.logging.log4j.LogManager
 
 class EnergyCalculatorImpl(val resUsageManager: ResourceUsageManager,
@@ -16,15 +19,48 @@ class EnergyCalculatorImpl(val resUsageManager: ResourceUsageManager,
     override fun calculateEnergyForRequests(): Map<String, String> {
         val traces = traceManager.getTraces()
 
-        _log.warn("Ignoring architecture description with ${archDesc.getStructure().elements.size} elements")
+        val augmentedTraces = addSyntheticSpansForArchitecturalElements(traces, archDesc.getStructure(), netInfo)
 
         var estimates = HashMap<String, String>()
-        for (t in traces) {
+        for (t in augmentedTraces) {
             _log.info("Calculating resource usage for trace ${t.root.spanId} (network address ${t.root.networkAddress})")
             val energyEstimate = calculateEnergyForRequest(t, resUsageManager)
             estimates[t.root.traceId] = energyEstimate
         }
         return estimates
+    }
+
+    fun addSyntheticSpansForArchitecturalElements(traces : List<Trace>, archElements : ArchitecturalDescription, netInfo: NetInfo) : List<Trace> {
+        var retVal = mutableListOf<Trace>()
+        var syntheticCounter = 256 ; // Start synthetic ids at 0x100
+        for (t in traces) {
+            var spans = mutableSetOf<Span>()
+            for (s in t.spans) {
+                spans.add(s)
+                val spanContainerName = netInfo.getNameForContainerAddress(s.networkAddress)
+                        ?: throw IllegalStateException("Could not find container name for network address ${s.networkAddress}")
+                val element = archElements.findElementByName(spanContainerName)
+                        ?: throw IllegalStateException("Could not find architecture element named $spanContainerName")
+                val containerNames = getContainerNamesForSpans(t.findChildrenOfSpan(s))
+                val subComponentNames = element.subComponents.map {it.name}
+                val missingElementNames = subComponentNames.minus(containerNames)
+                for (elementName in missingElementNames) {
+                    val networkAddress = netInfo.getAddressForContainerName(elementName)
+                            ?: throw IllegalStateException("Could not find address for container named $elementName")
+                    val syntheticId = (syntheticCounter++).toString(16)
+                    val syntheticSpan = Span(s.traceId, syntheticId, networkAddress,
+                            s.startTimeMsec, s.endTimeMsec, s.spanId)
+                    _log.info("Adding Synthetic Span $syntheticId for $networkAddress ($elementName) in span ${s.spanId}")
+                    spans.add(syntheticSpan)
+                }
+            }
+            retVal.add(Trace(spans))
+        }
+        return retVal
+    }
+
+    fun getContainerNamesForSpans(spans : Set<Span>) : List<String> {
+        return spans.map{netInfo.getNameForContainerAddress(it.networkAddress) ?: ""}.filter{it.length > 0}
     }
 
     fun calculateEnergyForRequest(t : Trace, resourceUsageMgr: ResourceUsageManager) : String {
