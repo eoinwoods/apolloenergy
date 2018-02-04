e7f47d7bc789f2bffce937c0d7b8d0629c02eb83
eoinwoods <eoin.woods@artechra.com>
2018-02-04 15:55:53 +0000

    Add missing negative tests

--- src/test/kotlin/com/artechra/apollo/netinfo/TestNetInfoDockerJsonImpl.kt
+++ src/test/kotlin/com/artechra/apollo/netinfo/TestNetInfoDockerJsonImpl.kt
@@ -2,6 +2,7 @@ package com.artechra.apollo.netinfo
 
 import com.artechra.apollo.integration.IntegrationTestConstants
 import com.artechra.apollo.util.TestUtil.Companion.getDataFilePath
+import junit.framework.TestCase.assertNull
 import org.junit.Test
 import kotlin.test.assertEquals
 
@@ -40,6 +41,30 @@ class TestNetInfoDockerJsonImpl {
     }
 
     @Test
+    fun testNonExistentNetworkAddressReturnsNullContainer() {
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
+        assertNull(obj.getContainerIdWithAddress("1.1.1.1"))
+    }
+
+    @Test
+    fun testNonExistentNameReturnsNullContainer() {
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
+        assertNull(obj.getContainerIdWithName("NoSuchContainer"))
+    }
+
+    @Test
+    fun testNonExistentContainerIdReturnsNullName() {
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
+        assertNull(obj.getNameForContainerId("a34488c68b81c5b07fcdf81d6a691b4b462018b62437ab87386b297a95e775FF"))
+    }
+
+    @Test
+    fun testNonExistentContainerIdReturnsNullAddress() {
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
+        assertNull(obj.getAddressForContainerId("a34488c68b81c5b07fcdf81d6a691b4b462018b62437ab87386b297a95e775FF"))
+    }
+
+    @Test
     fun testContainerNameResolvesToCorrectContainer() {
         val obj = NetInfoDockerJsonImpl(defaultDataFile())
         val gatewayName = obj.getContainerIdWithName("gateway")
@@ -59,7 +84,18 @@ class TestNetInfoDockerJsonImpl {
         val obj = NetInfoDockerJsonImpl(defaultDataFile())
         val gatewayName = obj.getNameForContainerAddress("172.18.0.7")
         assertEquals("gateway", gatewayName, "Wrong name for Gateway container")
+    }
 
+    @Test
+    fun testNonExistentAddressReturnsNullName() {
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
+        assertNull(obj.getNameForContainerAddress("1.1.1.1"))
+    }
+
+    @Test
+    fun testNonExistentNameReturnsNullAddress() {
+        val obj = NetInfoDockerJsonImpl(defaultDataFile())
+        assertNull(obj.getAddressForContainerName("NoSuchContainer"))
     }
 
 }
