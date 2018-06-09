1062020df8e48434b6d8e9a95e4f16cd11f69b84
eoinwoods <eoin.woods@artechra.com>
2018-06-09 09:37:54 +0100

    Remove obsolete energy model

--- src/main/kotlin/com/artechra/apollo/energymodel/EnergyModel.kt
+++
@@ -1,5 +0,0 @@
-package com.artechra.apollo.energymodel
-
-interface EnergyModel {
-    fun calculateEnergyInJoules(cpuUsageNanos : Long, memUsageMb : Long, diskIoBytes : Long, netIoBytes : Long) : Long
-}
--- src/main/kotlin/com/artechra/apollo/energymodel/TrivialEnergyModel.kt
+++
@@ -1,9 +0,0 @@
-package com.artechra.apollo.energymodel
-
-class TrivialEnergyModel(val cpuTdcW : Double, val diskUsageW : Double, val diskSpeedMb : Double,
-                         val memoryUsageWGb : Double, val netUsageWMb : Double) : EnergyModel {
-
-    override fun calculateEnergyInJoules(cpuUsageNanos: Long, memUsageMb: Long, diskIoBytes: Long, netIoBytes: Long): Long {
-        TODO("not implemented") //To change body of created functions use File | Settings | File Templates.
-    }
-}
