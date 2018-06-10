9790a05a7afa1de99f91b31810d5220b95e0f36c
eoinwoods <eoin.woods@artechra.com>
2018-06-10 11:49:39 +0100

    Skeleton rework of TraceCalculator

--- src/main/kotlin/com/artechra/apollo/calculator/TraceCalculator.kt
+++ src/main/kotlin/com/artechra/apollo/calculator/TraceCalculator.kt
@@ -2,10 +2,44 @@ package com.artechra.apollo.calculator
 
 import com.artechra.apollo.netinfo.NetInfo
 import com.artechra.apollo.resusage.ResourceUsageManager
-import com.artechra.apollo.types.ResourceUsage
-import com.artechra.apollo.types.Trace
+import com.artechra.apollo.types.*
+import kotlin.math.roundToLong
 
 class TraceCalculator(val trace : Trace, val resourceUsageMgr : ResourceUsageManager, val networkMap : NetInfo) {
+    val containers = networkMap.getContainersForAddresses()
+
+    fun calculateEnergyEstimateForTrace(t : Trace) : Long {
+        var estimateJoules = 0L
+        for (span in t.spans) {
+            val containerUsage = calculateResourceUsageForSpan(span)
+            val hostCpuMsecUsage = getHostResourceUsageForSpan(span)
+            val hostEnergyUsage = getEnergyJoulesForHost(span.networkAddress, span.startTimeMsec, span.endTimeMsec)
+
+            assert(containerUsage.usage.totalCpuMsec > 0)
+            assert(hostCpuMsecUsage.cpuUsageMsec > 0)
+            val containerUsagePercentage = containerUsage.usage.totalCpuMsec / (hostCpuMsecUsage.cpuUsageMsec*1.0)
+            val spanEnergyJoules = hostEnergyUsage * containerUsagePercentage
+            estimateJoules += spanEnergyJoules.roundToLong()
+        }
+        return estimateJoules
+    }
+
+    fun calculateResourceUsageForSpan(s : Span) : ResourceUsageMeasurement {
+        val containerId =
+                containers.get(s.networkAddress)
+                        ?: throw IllegalStateException("No container found for ipAddr " + s.networkAddress + " in span " + s.spanId)
+        val resourceMetrics =
+                resourceUsageMgr.getResourceUsage(containerId, s.startTimeMsec, s.endTimeMsec)
+        return resourceMetrics
+    }
+
+    fun getHostResourceUsageForSpan(s : Span) : HostResourceMeasurement {
+        return resourceUsageMgr.getHostResourceUsage(s.networkAddress, s.startTimeMsec, s.endTimeMsec)
+    }
+
+    fun getEnergyJoulesForHost(hostName : String, startTimeMsec : Long, endTimeMsec : Long) : Long {
+        return 100
+    }
 
     fun calculateTotalResourceUsage() : ResourceUsage {
         var totalCpuTicks    = 0L
@@ -20,7 +54,7 @@ class TraceCalculator(val trace : Trace, val resourceUsageMgr : ResourceUsageMan
                             throw IllegalStateException("No container found for ipAddr " + span.networkAddress + " in span " + span.spanId)
             val resourceMetrics =
                     resourceUsageMgr.getResourceUsage(containerId, span.startTimeMsec, span.endTimeMsec)
-            totalCpuTicks    += resourceMetrics.usage.totalCpuTicks
+            totalCpuTicks    += resourceMetrics.usage.totalCpuMsec
             totalMemoryBytes    += resourceMetrics.usage.totalMemoryBytes
             totalDiskIoBytes += resourceMetrics.usage.totalDiskIoBytes
             totalNetIoBytes  += resourceMetrics.usage.totalNetIoBytes
--- src/test/kotlin/com/artechra/apollo/calculator/TestTraceCalculator.kt
+++ src/test/kotlin/com/artechra/apollo/calculator/TestTraceCalculator.kt
@@ -21,9 +21,25 @@ class TestTraceCalculator {
         val nullTrace = Trace(setOf(Span("54C92796854B15C8","54C92796854B15C8", "192.168.1.2",baseTime+100000, baseTime+110000)))
         val tc = TraceCalculator(nullTrace, emptyResourceUsageManager, emptyNetInfo)
         val resUsage = tc.calculateTotalResourceUsage()
-        assertEquals("Wrong CPU total", 1, resUsage.totalCpuTicks)
+        assertEquals("Wrong CPU total", 1, resUsage.totalCpuMsec)
         assertEquals("Wrong memory total", 2, resUsage.totalMemoryBytes)
         assertEquals("Wrong diskio total", 3, resUsage.totalDiskIoBytes)
         assertEquals("Wrong netio total", 4, resUsage.totalNetIoBytes)
+    }
+
+    @Test
+    fun trivialTraceShouldReturnRootSpanEstimate() {
+        val baseTime = 1515237362000
+        val networkAddr = "192.168.1.2"
+        val containerId = "abcdefghi123"
+        val testNetInfo = StubNetInfo(hashMapOf(networkAddr to containerId),
+                hashMapOf(containerId to networkAddr))
+        val testResourceUsageManager = StubResourceUsageManager(hashMapOf(containerId to ResourceUsage(12, 2, 3, 4)),
+                hashMapOf(networkAddr to 123L))
+        val trivialTrace = Trace(setOf(Span( "54C92796854B15C8","54C92796854B15C8", "192.168.1.2",baseTime+100000, baseTime+110000)))
+        val tc = TraceCalculator(Trace(setOf<Span>(Span("AA", "AA", "1.1.1.1", 1515237362000, 1515237362000))), testResourceUsageManager, testNetInfo)
+        val estimate = tc.calculateEnergyEstimateForTrace(trivialTrace) ;
+        assertEquals(10, estimate)
+
     }
 }
