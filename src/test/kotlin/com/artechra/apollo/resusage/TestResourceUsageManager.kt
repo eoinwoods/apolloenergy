2c99a474bc3d6413b4c1eff05c2c0c8cb5c3a273
eoinwoods <eoin.woods@artechra.com>
2018-06-16 22:33:43 +0100

    Remove compiler warning

--- src/test/kotlin/com/artechra/apollo/resusage/TestResourceUsageManager.kt
+++ src/test/kotlin/com/artechra/apollo/resusage/TestResourceUsageManager.kt
@@ -8,7 +8,7 @@ class TestResourceUsageManager {
     @Test
     fun theNullImplementationShouldReturnAnItem() {
         val cid = "b38b452428f79542a48a01b786b24bceed82bd2b55650db649bdc622eb27f66a"
-        val resUsage = ResourceUsageManagerDefaultImplementation().getResourceUsage(cid, 100, 200)!!
+        val resUsage = ResourceUsageManagerDefaultImplementation().getResourceUsage(cid, 100, 200)
         assertTrue(resUsage.usage.totalCpuMsec  + resUsage.usage.totalMemoryBytes + resUsage.usage.totalDiskIoBytes + resUsage.usage.totalNetIoBytes > 0)
     }
 }
