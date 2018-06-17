b1489c07ff6b871ba4e0c62aefc3b6a96b554366
eoinwoods <eoin.woods@artechra.com>
2018-06-17 12:33:14 +0100

    Address warnings

--- src/test/kotlin/com/artechra/apollo/integration/TestInfluxDbAccess.kt
+++ src/test/kotlin/com/artechra/apollo/integration/TestInfluxDbAccess.kt
@@ -5,19 +5,19 @@ import org.influxdb.dto.Query
 import org.junit.Test
 import kotlin.test.assertTrue
 
-class TestInfluxDbAccess {
+const val DB_URL = "http://localhost:8086"
 
-    val DB_URL = "http://localhost:8086"
+class TestInfluxDbAccess {
 
     @Test
     fun testConnectionIsSuccessful() {
-        var influxDB = InfluxDBFactory.connect(DB_URL)
+        val influxDB = InfluxDBFactory.connect(DB_URL)
         influxDB.close()
     }
 
     @Test
     fun testThatQueryReturnsData() {
-        var influxDB = InfluxDBFactory.connect(DB_URL)
+        val influxDB = InfluxDBFactory.connect(DB_URL)
         val query = Query("SELECT time, NumGC, hostname FROM runtime LIMIT 1", "_internal")
         val result = influxDB.query(query)
         assertTrue(result.results[0].series[0].values[0].size == 3)
