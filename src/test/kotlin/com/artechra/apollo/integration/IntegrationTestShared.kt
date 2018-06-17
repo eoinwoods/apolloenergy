af536c2309ca58a6f9d8e30b83cd00afc2afe9ba
eoinwoods <eoin.woods@artechra.com>
2018-06-17 12:31:56 +0100

    Address warnings

--- src/test/kotlin/com/artechra/apollo/integration/IntegrationTestShared.kt
+++ src/test/kotlin/com/artechra/apollo/integration/IntegrationTestShared.kt
@@ -12,17 +12,17 @@ class IntegrationTestShared {
         const val MYSQL_DRIVER = "com.mysql.cj.jdbc.Driver"
         const val MYSQL_DB   = "zipkin"
         const val MYSQL_URL  = "jdbc:mysql://localhost" + "/" + MYSQL_DB
-        val MYSQL_USER = "zipkin"
-        val MYSQL_PASS = "zipkin"
+        const val MYSQL_USER = "zipkin"
+        const val MYSQL_PASS = "zipkin"
 
         // These values correspond to data in test data set '20180604-v2-cpu-data-mix'
-        val TEST_SET_NAME = "20180610-cpu-data-mix"
-        val GATEWAY_CONTAINER_ID = "ffd31b51662c9e786bc476329474ed51b4ba780652bebd9f8b73eee7915d7da2"
-        val INFLUXDB_CONTAINER_ID = "7a822ae188738e97b404f7fd4f5249676d41763c8b634f2055c139781e7347fe"
-        val SPAN_START_TIME_MS = 1528666358000
-        val SPAN_END_TIME_MS   = 1528666372000
-        val HOST_NAME = "7a822ae18873"
-        val MULTI_SPAN_ZIPKIN_TRACE_ID = "468CE081D5B05907"
+        const val TEST_SET_NAME = "20180610-cpu-data-mix"
+        const val GATEWAY_CONTAINER_ID = "ffd31b51662c9e786bc476329474ed51b4ba780652bebd9f8b73eee7915d7da2"
+        const val INFLUXDB_CONTAINER_ID = "7a822ae188738e97b404f7fd4f5249676d41763c8b634f2055c139781e7347fe"
+        const val SPAN_START_TIME_MS = 1528666358000
+        const val SPAN_END_TIME_MS   = 1528666372000
+        const val HOST_NAME = "7a822ae18873"
+        const val MULTI_SPAN_ZIPKIN_TRACE_ID = "468CE081D5B05907"
 
         fun getTestDataSetLoadedName(dbConn : InfluxDbDecoratorImpl, databaseName : String) : String {
             val query = Query("SELECT * FROM apollo_check WHERE value = 1", databaseName)
--- src/test/kotlin/com/artechra/apollo/integration/TestMySqlZipkinTraceManager.kt
+++ src/test/kotlin/com/artechra/apollo/integration/TestMySqlZipkinTraceManager.kt
@@ -9,11 +9,10 @@ import org.springframework.jdbc.core.JdbcTemplate
 import org.springframework.jdbc.datasource.DriverManagerDataSource
 import javax.sql.DataSource
 
+const val MULTI_SPAN_TRACE_ID = IntegrationTestShared.MULTI_SPAN_ZIPKIN_TRACE_ID
+const val TEST_DATA_SET = IntegrationTestShared.TEST_SET_NAME
 
 class TestMySqlZipkinTraceManager {
-
-    val MULTI_SPAN_TRACE_ID = IntegrationTestShared.MULTI_SPAN_ZIPKIN_TRACE_ID
-    val TEST_DATA_SET = IntegrationTestShared.TEST_SET_NAME
 
     @Before
     fun checkPreconditions() {
