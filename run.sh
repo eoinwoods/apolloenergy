4cd165445e0a74e4743956086ea4504728403cdf
eoinwoods <eoin.woods@artechra.com>
2018-07-03 23:14:08 +0100

    Added optional Log4J config

--- run.sh
+++ run.sh
@@ -3,4 +3,5 @@
 # Simple run script for EnergyApollo to run it via the "fat" jar and using
 # the default properties file
 #
-java -jar build/libs/EnergyApollo-all.jar etc/apollo.properties
+#logging_config=-Dlog4j.configurationFile=etc/log4j.xml
+java $logging_config -jar build/libs/EnergyApollo-all.jar etc/apollo.properties
