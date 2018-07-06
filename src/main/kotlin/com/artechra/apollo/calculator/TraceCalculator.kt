38d81a65fbe9cdb7f42ceed102c33beb78584441
eoinwoods <eoin.woods@artechra.com>
2018-07-06 22:38:19 +0100

    New calculation algorithm or v3

--- src/main/kotlin/com/artechra/apollo/calculator/TraceCalculator.kt
+++ src/main/kotlin/com/artechra/apollo/calculator/TraceCalculator.kt
@@ -14,41 +14,31 @@ class TraceCalculator(private val resourceUsageMgr : ResourceUsageManager,
 
     private val containers = networkMap.getContainersForAddresses()
 
+    fun calculateResourceUsageForContainer(containerId : String, startTimeMsec : Long, endTimeMsec : Long) : ResourceUsageMeasurement {
+        return resourceUsageMgr.getResourceUsage(containerId, startTimeMsec, endTimeMsec)
+    }
+
     fun calculateCpuMsecAndEnergyJoulesEstimateForTrace(t : Trace) : EnergyEstimate {
         var estimateJoules = 0L
         var totalCpuMsec = 0L
-        for (span in t.spans) {
-            val containerUsage = calculateResourceUsageForSpan(span)
-            totalCpuMsec += containerUsage.usage.totalCpuMsec
-            _log.debug("Calculated container usage of ${containerUsage} ")
+        val containerIds = t.spans.map {s -> s.networkAddress}.map {a -> getContainerForNetworkAddress(a)}
 
-            val hostCpuMsecUsage = getHostResourceUsageForSpan(span)
-            val hostEnergyUsage = getEnergyJoulesForHostForSpan(span)
-            _log.debug("Calculated host CPU usage of ${hostCpuMsecUsage} ms and host energy of ${hostEnergyUsage}J")
+        val containerUsage = containerIds.map {id -> id to calculateResourceUsageForContainer(id, t.getStartTime(), t.getEndTime())}.toMap()
+        val hostUsage = containerIds.map {id -> id to resourceUsageMgr.getHostResourceUsageForContainer(id, t.getStartTime(), t.getEndTime())}.toMap()
+        val hostEnergy = containerIds.map { id -> id to energyManager.getEnergyUsageForHostForContainerInJoules(id, t.getStartTime(), t.getEndTime())}.toMap()
 
-            assert(containerUsage.usage.totalCpuMsec > 0)
-            assert(hostCpuMsecUsage.cpuUsageMsec > 0)
-            val containerUsagePercentage = containerUsage.usage.totalCpuMsec / (hostCpuMsecUsage.cpuUsageMsec*1.0)
-            val spanEnergyJoules = hostEnergyUsage * containerUsagePercentage
-            _log.debug("Span ${span.spanId}: cpuMsec=${containerUsage.usage.totalCpuMsec}; hostCpuMsec=${hostCpuMsecUsage.cpuUsageMsec}; hostEnergyJ=$hostEnergyUsage; usage%=$containerUsagePercentage spanEnergyJ=$spanEnergyJoules")
-            estimateJoules += spanEnergyJoules.roundToLong()
+        for (id in containerIds) {
+            val containerCpuMsec = containerUsage[id]!!.usage.totalCpuMsec
+            val hostCpuMsecForContainer = hostUsage[id]!!.cpuUsageMsec
+            totalCpuMsec += containerCpuMsec
+            val containerUsagePercentage = containerCpuMsec /
+                    (hostCpuMsecForContainer*1.0)
+            val containerEnergyJ = hostEnergy[id]!! * containerUsagePercentage
+            estimateJoules += containerEnergyJ.roundToLong()
+            _log.debug("Container $id totalCpuMsec=$containerCpuMsec hostCpuMsec=$hostCpuMsecForContainer containerUsage=$containerUsagePercentage containerEnergy=$containerEnergyJ")
         }
+
         return EnergyEstimate(totalCpuMsec, estimateJoules, t.name)
-    }
-
-    private fun calculateResourceUsageForSpan(s : Span) : ResourceUsageMeasurement {
-        val containerId = getContainerForNetworkAddress(s.networkAddress)
-        return resourceUsageMgr.getResourceUsage(containerId, s.startTimeMsec, s.endTimeMsec)
-    }
-
-    private fun getHostResourceUsageForSpan(s : Span) : HostResourceMeasurement {
-        val containerId = getContainerForNetworkAddress(s.networkAddress)
-        return resourceUsageMgr.getHostResourceUsageForContainer(containerId, s.startTimeMsec, s.endTimeMsec)
-    }
-
-    private fun getEnergyJoulesForHostForSpan(s : Span) : Long {
-        val containerId = getContainerForNetworkAddress(s.networkAddress)
-        return energyManager.getEnergyUsageForHostForContainerInJoules(containerId, s.startTimeMsec, s.endTimeMsec)
     }
 
     private fun getContainerForNetworkAddress(address : String) : String {
---
+++ src/test/kotlin/com/artechra/apollo/util/TestReduce.kt
@@ -0,0 +1,14 @@
+package com.artechra.apollo.util
+
+import kotlin.test.Test
+
+class TestReduce {
+    @Test
+    public fun testReduce() {
+       data class Item(val id : Int, val value : Double)
+
+        val itemList = listOf(Item(1, 100.0), Item(2, 101.0), Item(3, 102.0))
+
+        println(itemList.map({i -> i.value}).reduce({v,w -> v+w}))
+    }
+}
