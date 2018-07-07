625e17e0dd64ea49ee4b947099f898a7cdc3d524
eoinwoods <eoin.woods@artechra.com>
2018-07-07 17:22:58 +0100

    Allow multiple datasets

--- run-dataset.sh
+++ run-dataset.sh
@@ -8,28 +8,30 @@ DATADIR=$DIR/data
 
 if [ "$#" -lt 1 ]
 then
-   echo USAGE: $0 data-set-name
+   echo USAGE: $0 data-set-name [data-set-name [...]]
    exit 1
 fi
-data_set=$1
 
-if [ ! -d "$DATADIR/$data_set" -a ! -f "$DATADIR/${data_set}.tar" ]
-then
-   echo "No sign of data set $data_set as a directory or tar(1) file"
-   exit 2
-fi
+for data_set in $*
+do
+	echo "Data Set $data_set"
+	if [ ! -d "$DATADIR/$data_set" -a ! -f "$DATADIR/${data_set}.tar" ]
+	then
+	   echo "No sign of data set $data_set as a directory or tar(1) file"
+	   exit 2
+	fi
 
-(cd $DATADIR && $DATADIR/load_dataset.sh $data_set)
+	(cd $DATADIR && $DATADIR/load_dataset.sh $data_set)
 
-if [ ! -d "$DATADIR/$data_set" ]
-then
-   $DATA_DIR/unpack_dataset.sh $data_set
-fi
+	if [ ! -d "$DATADIR/$data_set" ]
+	then
+	   $DATA_DIR/unpack_dataset.sh $data_set
+	fi
 
-#logging_config=-Dlog4j.configurationFile=etc/log4j.xml
-# -Dapollo.network.info.filename=abc123
-java $logging_config  \
-    -Dapollo.network.info.filename=$DATADIR/$data_set/docker_network.json \
-    -jar build/libs/EnergyApollo-all.jar \
-    etc/apollo.properties
-
+	#logging_config=-Dlog4j.configurationFile=etc/log4j.xml
+	# -Dapollo.network.info.filename=abc123
+	java $logging_config  \
+	    -Dapollo.network.info.filename=$DATADIR/$data_set/docker_network.json \
+	    -jar build/libs/EnergyApollo-all.jar \
+	    etc/apollo.properties
+done
