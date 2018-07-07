fd433ade91555addcb20e84c5eb959ab0ab52e2c
eoinwoods <eoin.woods@artechra.com>
2018-07-07 16:52:50 +0100

    Address warnings

--- src/test/kotlin/com/artechra/apollo/util/TestReduce.kt
+++ src/test/kotlin/com/artechra/apollo/util/TestReduce.kt
@@ -4,11 +4,11 @@ import kotlin.test.Test
 
 class TestReduce {
     @Test
-    public fun testReduce() {
+    fun testReduce() {
        data class Item(val id : Int, val value : Double)
 
         val itemList = listOf(Item(1, 100.0), Item(2, 101.0), Item(3, 102.0))
 
-        println(itemList.map({i -> i.value}).reduce({v,w -> v+w}))
+        println(itemList.map{i -> i.value}.reduce{v,w -> v+w})
     }
 }
